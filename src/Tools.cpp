afbc7e27736f38a930d77aa1c53f3737cfa4c11d
Frankie Dintino <fdintino@gmail.com>
2010-05-27 19:54:18 +0000

    XyDiff delta's text update "pos" and "len" attributes c data type should be changed from int to XMLSize_t

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -85,12 +85,6 @@ int watoi(const XMLCh* str) {
 	return intValue;
 }
 
-const XMLCh * witoa(int intValue)
-{
-	const XMLCh *str = XMLString::transcode( itoa(intValue).c_str() );
-	return str;
-}
-
 const XMLCh * witoa(XMLSize_t intValue)
 {
 	const XMLCh *str = XMLString::transcode( itoa(intValue).c_str() );
@@ -107,31 +101,6 @@ bool existsFile(const char *fileName) {
 		fclose(testFile);
 	}
 	return (testFile != NULL);
-}
-
-std::string itoa (int n)
-{	
-	char * s = new char[17];
-	std::string u;
-	
-	if (n < 0) {         //turns n positive
-		n = (-1 * n); 
-		u = "-";         //adds '-' on result string
-	}
-	
-	int i = 0;  // s counter
-	
-	do {
-		s[i++] = n % 10 + '0'; //conversion of each digit of n to char
-		n -= n % 10;           //update n value
-	} while ((n /= 10) > 0);
-	
-	for (int j = i-1; j >= 0; j--) { 
-		u += s[j];    //building our string number
-	}
-	
-	delete[] s;       //free-up the memory!
-	return u;
 }
 
 std::string itoa (XMLSize_t n)
@@ -157,17 +126,6 @@ std::string itoa (XMLSize_t n)
 	
 	delete[] s;       //free-up the memory!
 	return u;
-}
-
-
-int intmin(int x, int y)
-{
-	return (x < y) ? x : y;
-}
-
-int intmax(int x, int y)
-{
-	return (x > y) ? x : y;
 }
 
 XMLSize_t intmax(XMLSize_t x, XMLSize_t y)
--- src/Tools.hpp
+++ src/Tools.hpp
@@ -55,7 +55,6 @@ bool isDelta(const xercesc::DOMDocument *doc) ;
 
 int watoi(const XMLCh* str);
 
-const XMLCh * witoa(int intValue);
 const XMLCh * witoa(XMLSize_t intValue);
 /*
  * tells if a certain file exists or not 
@@ -66,9 +65,9 @@ bool existsFile(const char *fileName);
 /*
  * Converts int to std::string
  */
-std::string itoa (int n);
-int intmin(int x, int y);
-int intmax(int x, int y);
+std::string itoa (XMLSize_t n);
+XMLSize_t intmin(XMLSize_t x, XMLSize_t y);
+XMLSize_t intmax(XMLSize_t x, XMLSize_t y);
 
 #if defined(_WIN32) || defined(_WIN64)
 #ifdef NATIVEDLL_EXPORTS
